70404456bcdfb86469b5eca1b7aa7b0d433f2fa3
Greenstar <greenstar1151@gmail.com>
2020-04-07 19:45:48 +0900

    solved 2153

--- 2153_소수 단어/2153_소수 단어_200407.py
+++ 2153_소수 단어/2153_소수 단어_200407.py
@@ -15,6 +15,7 @@ def is_prime(n: int) -> bool:
         k += 6
     return True
 
+
 alphabet_index = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 
 word_in = input()
