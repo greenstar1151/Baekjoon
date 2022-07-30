f110436585029a89d6ec26e6e1989bd92ba9feae
greenstar1151 <39073901+greenstar1151@users.noreply.github.com>
2022-07-30 22:18:54 +0900

    (Up)Solved 1790 with Python3

--- 1790_수 이어 쓰기 2/1790_수 이어 쓰기 2.py
+++ 1790_수 이어 쓰기 2/1790_수 이어 쓰기 2.py
@@ -1,47 +1,16 @@
-import math
-
-def numlen_prev(x: int):
-    expX = math.floor(math.log10(x))
-    numlen_prev_x = 0
-    for i in range(expX):
-        if expX == 0:
-            return 0
-        else:
-            numlen_prev_x += 10**i * 9 * (i+1)
-    else:
-        return numlen_prev_x
-
-
 N, k = map(int, input().split())
+upper_bound = 0
+exps = 0
+while upper_bound < k:
+    digits = exps + 1
+    upper_bound += digits * 9 * 10 ** exps
+    exps += 1
 
-
-expN = math.floor(math.log10(N))
-expk = math.floor(math.log10(k))
-numlen_N_prev = numlen_prev(N)
-base10_k_prev = 0
-numlen_k_prev = 0
-
-
-for i in range(9):
-    numlen_k_prev += 10**i * 9 * (i+1)
-    base10_k_prev += 1
-    if numlen_k_prev >= k:
-        numlen_k_prev -= 10**i * 9 * (i+1)
-        base10_k_prev -= 1
-        break
-    else:
-        continue
-    
-# 1234567891011121314151617181920
-number = str(10**base10_k_prev + ((k - numlen_k_prev)//expk))
-#number = str((10**expk) + (math.floor((k - numlen_knum - 1) / (expk + 1))))
-numlen = numlen_N_prev + (N - 10**expN + 1)*(expN + 1)
-
-# debug
-print('base10_k_prev : {}, numlen : {}, numlen_k_prev : {}, number : {}' .format(base10_k_prev, numlen, numlen_k_prev, number))
-print('{} % {} = {}' .format((k - numlen_k_prev - 1), ((math.floor(math.log10(int(number)))) + 1), ((k - numlen_k_prev - 1) % ((math.floor(math.log10(int(number)))) + 1))))
-
-if k > numlen:
+lower_bound = upper_bound - (exps) * 9 * 10 ** (exps - 1)
+number_offset_from_lower, str_offset = divmod(k - lower_bound - 1, exps)
+lower_bound_number = 10 ** (exps - 1) - 1
+target_number = lower_bound_number + number_offset_from_lower + 1
+if target_number > N:
     print(-1)
 else:
-    print(number[((k - numlen_k_prev - 1) % ((math.floor(math.log10(int(number)))) + 1))])
+    print(str(lower_bound_number + number_offset_from_lower + 1)[str_offset])
