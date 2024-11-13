0fc99bca00c742e7661a181ffb71ce3fa7d14286
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2024-11-13 22:41:14 +0900

    Solved 9657 with Python3

---
+++ 9657_돌 게임 3/9657_돌 게임 3_241113.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def solution(N: int):
+    # DP[i][0] -> 돌이 i개 있을 때 상근이부터 시작해서 게임을 이기는지 여부(0: 짐, 1: 이김)
+    # DP[i][1] -> 돌이 i개 있을 때 창영이부터 시작해서 게임을 이기는지 여부
+    DP = [[-1, -1] for _ in range(N + 1)]
+
+    if N == 1:
+        return "SK"
+    elif N == 2:
+        return "CY"
+    elif N == 3 or N == 4:
+        return "SK"
+
+    DP[1][0], DP[1][1] = 1, 1
+    DP[2][0], DP[2][1] = 0, 0
+    DP[3][0], DP[3][1] = 1, 1
+    DP[4][0], DP[4][1] = 1, 1
+
+    for i in range(5, N + 1):
+        DP[i][0] = (
+            1 if DP[i - 4][1] == 0 or DP[i - 3][1] == 0 or DP[i - 1][1] == 0 else 0
+        )
+        DP[i][1] = (
+            1 if DP[i - 4][0] == 0 or DP[i - 3][0] == 0 or DP[i - 1][0] == 0 else 0
+        )
+
+    return "SK" if DP[N][0] == 1 else "CY"
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline().rstrip())
+    print(solution(N))
